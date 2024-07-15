24bb77446dc1dac768826df8386b322e05642690
Kelly <kellywang284@gmail.com>
2024-07-15 23:09:54 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import random
+
+for i in range(3):
+    print(random.randint(10, 20))
+
+for i in range(3):
+    print(random.random())
+
+members = ["Hazel", "Mary", "Reyna"]
+leader = random.choice(members)
+print(f"The leader is {leader}")
+
+
+class Dice:
+    def roll(self):
+        first = random.randint(1, 6)
+        second = random.randint(1, 6)
+        return first, second
+
+
+dice = Dice()
+print(dice.roll())
